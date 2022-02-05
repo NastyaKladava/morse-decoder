bd09bbaf08817522b6facc457c2de33aab7d88ad
Nastya Kladova <nstsologub@gmail.com>
2022-02-05 22:54:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,26 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+  function decode(expr) {
+        let result = "";
+
+        for (let i = 0; i < expr.length; i += 10) {
+          let numWord = expr.substr(i, 10);
+          let morseLetter = "";
+
+          if (numWord === "**********") {
+            result += " ";
+          } else {
+            for (let j = 0; j < expr.length; j += 2) {
+              let numLetter = numWord.substr(j, 2);
+              if (numLetter === "10") morseLetter += ".";
+              if (numLetter === "11") morseLetter += "-";
+            }
+            result += MORSE_TABLE[morseLetter];
+          }
+        }
+        return result;
+      }
 
 module.exports = {
     decode
